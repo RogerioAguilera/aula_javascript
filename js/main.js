c7f96115be71e1c313359ed82902ba8f15ff43ba
Rogerio Aguilera <raguileraribeiro33@gmail.com>
2020-11-02 23:54:06 -0300

    Novas alteracoes

---
+++ js/main.js
@@ -0,0 +1,105 @@
+
+function clicou(){
+
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    // console.log(document.getElementById("agradecimento"));
+    // alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.location.href = "https://digitalinnovation.one/";
+}
+
+function trocar(elemento){
+    // document.getElementById("mousemove").innerHTML ="Obrigado por colaborar";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+
+}
+
+function voltar(elemento){
+
+    // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("página carregada");
+}
+
+function funcaoChange(elemento);
+ console.log(elemento.value);
+// function soma(n1, n2){
+//     return n1+n2;
+
+// }
+
+// var validar = 0;
+ 
+// function validaIdade(idade){
+//     var validar;
+//     if(idade >=18){
+//         validar = true
+//     }else{
+//     validar = false;
+//     }
+
+//     return validar;
+// }
+
+// var idade = prompt("Qual sua idade");
+// console.log(validaIdade(idade));
+
+// var idade = prompt("Qual sua idade");
+// console.log(validaIdade(idade));
+
+
+
+// var trajeto = ["mãe", "floresta", "avó"];
+// trajeto.push("lobo");
+
+// var d = new Date();
+// alert(d.getDay());
+// alert(d.getHours());
+// alert(d.getMinutes());
+
+// var count;
+// for(count =0, count <=5; count++);
+// alert(count);
+
+// var count = 5;
+// while(count<=5){
+
+//     console.log(count);
+//     count++
+// };
+
+
+
+// var idade = prompt("Qual sua idade");
+// if(idade >=18){
+//     alert("maior de idade");
+
+// }else{
+//     alert("menor de idade");
+// }
+
+// var frutas =  [{nome:"maca", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+// var lista = ["Pera", "Uva", "Morango", "Melancia"];
+// lista.push("uva");
+
+// console.log(lista.toString());
+// console.log(lista.join(" ** "));
+
+
+// var nome = "Rogério Ribeiro";
+// var idade = 33;
+// var frase = "Japão é o melhor time do mundo";
+// //alert(nome + " tem " +idade);
+
+// console.log(nome);
+// console.log(idade);
+// console.log(frase.replace("Japão", "Alemanha"));
+// console.log(frase.toUpperCase());
